2061ab13f37b7ea01b924c171a332f39a4f5bd0e
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2020-07-08 17:28:57 +0530

    date picker method

---
+++ src/test/java/com/Conceptunderstanding/DatePicker/DatePickerTest.java
@@ -0,0 +1,85 @@
+/**
+ * @author 			:	 Neelam-Sonu
+ *	DATE       		:	 08-Jul-2020
+ *  FILE NAME  		: 	 DatePickerTest.java
+ *  PROJECT NAME 	:	 SeleniumInterviewPractice
+ * 	Class Time		:    4:57:56 pm
+ */
+package com.Conceptunderstanding.DatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import freemarker.template.SimpleDate;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DatePickerTest 
+{
+	public static void main(String[] args) throws ParseException {
+		
+		WebDriver driver; 
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver= new ChromeDriver();
+		
+		driver.navigate().to("https://jqueryui.com/datepicker/");
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+		
+		driver.findElement(By.xpath("//input[@id='datepicker'][1]")).click();
+		
+		String setDateStr= "09/08/2020";
+		
+		String currDateStr = driver.findElement(By.xpath("//div[@class='ui-datepicker-title']")).getText(); //MMMM YYYY
+		
+		System.out.println(currDateStr);
+		
+		Date setDate = new SimpleDateFormat("dd/mm/yyyy").parse(setDateStr);
+		
+		Date currDate = new SimpleDateFormat("MMMM yyyy").parse(currDateStr);
+		
+		//fetch the different between current month and desire month
+		int monthDiff = Months.monthsBetween(new DateTime(currDate).withDayOfMonth(1), new DateTime(setDate).withDayOfMonth(1)).getMonths();
+				
+		boolean isFuture = true; 
+		
+		//if month is 0 then convert in to positive for run loop
+		if(monthDiff<0)
+		{
+			isFuture=false;
+				monthDiff = -1 * monthDiff;
+		}
+		
+		for(int i=0; i<monthDiff; i++)
+		{
+			if(isFuture)
+			{
+				//click on next bar
+				driver.findElement(By.xpath("//a[@title='Next'][1]")).click();
+			}
+			else
+			{
+				//click on previous bar
+				driver.findElement(By.xpath("//a[@title='Prev'][1]")).click();
+				
+			}
+			
+			String day; 
+			
+			day = new SimpleDateFormat("dd").format(setDate);
+			
+			System.out.println(day);
+			driver.findElement(By.xpath("//a[text()='"+Integer.parseInt(day)+"']")).click();
+		}
+	}
+
+}
